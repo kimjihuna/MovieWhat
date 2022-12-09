e7ac9aba7f1e938495427c94345f0a6423823e56
김지훈 <@host.docker.internal>
2022-12-09 15:26:19 +0900

    test

---
+++ MovieWhat/src/main/java/kr/moviewhat/MovieWhatApplication.java
@@ -0,0 +1,13 @@
+package kr.moviewhat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieWhatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieWhatApplication.class, args);
+	}
+
+}
---
+++ MovieWhat/src/test/java/kr/moviewhat/MovieWhatApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.moviewhat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieWhatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
